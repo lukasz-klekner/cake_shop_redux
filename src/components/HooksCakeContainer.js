609b247507cce7955807d424c01b0be40b89d043
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-15 10:16:05 +0200

    uses useDispatch hook

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,13 +1,15 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { buyCake } from '../redux'
 
 const HooksCakeContainer = () => {
   const numberOfCakes = useSelector(({ numberOfCakes }) => numberOfCakes)
+  const dispatch = useDispatch()
+
   return (
     <div>
       <h2>Number of cakes - {numberOfCakes}</h2>
-      <button>Buy cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy cake</button>
     </div>
   )
 }
